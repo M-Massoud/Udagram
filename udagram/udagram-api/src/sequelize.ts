138a6a05895b9e355d5684f399c454e20b2fae6a
M-Massoud <mohamed97masoud97@gmail.com>
2022-10-14 03:41:13 +0200

    circle ci configs

--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -1,12 +1,15 @@
 import { Sequelize } from 'sequelize-typescript';
 import { config } from './config/config';
 
-export const sequelize = new Sequelize({
-  username: config.username,
-  password: config.password,
-  database: config.database,
-  host: config.host,
+// export const sequelize = new Sequelize({
+//   username: config.username,
+//   password: config.password,
+//   database: config.database,
+//   host: config.host,
 
-  dialect: 'postgres',
-  storage: ':memory:',
-});
+//   dialect: 'postgres',
+//   storage: ':memory:',
+// });
+export const sequelize = new Sequelize(
+  'postgresql://postgres:postgres@database-1.chr08mf7o9sx.us-east-1.rds.amazonaws.com:5432/postgres'
+);
