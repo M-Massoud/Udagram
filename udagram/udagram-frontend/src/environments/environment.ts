379aae70d700815dfc917f417a3d5ae215b68969
M-Massoud <mohamed97masoud97@gmail.com>
2022-10-15 15:18:06 +0200

    update env variables

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0',
+  apiHost:
+    'http://udagram-api-dev.eba-sqp9y2yp.us-east-1.elasticbeanstalk.com//api/v0',
 };
 
 /*
